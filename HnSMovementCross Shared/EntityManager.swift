e51965f15c2e5b3a1afd6f3e7aec8d0336b32dd0
Unknown <maurovelin@yahoo.com>
2018-04-17 21:24:26 +0200

    NANANANANA IT'S ME AND U NAGG DIJ

---
+++ HnSMovementCross Shared/EntityManager.swift
@@ -0,0 +1,33 @@
+//
+//  EntityManager.swift
+//  HnSMovementCross
+//
+//  Created by Mauro Romito on 17/04/18.
+//  Copyright © 2018 lollibus. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import  GameplayKit
+
+class EntityManager {
+    
+    var entities = Set<GKEntity>()
+    var scene: GameScene
+    
+    init(scene: GameScene){
+        self.scene = scene
+    }
+    
+    func addEntity (_ entity: GKEntity){
+        
+        entities.insert(entity)
+        
+        if let spriteComponent = entity.component(ofType: SpriteComponent.self) {
+            scene.addChild(spriteComponent.sprite)
+        }
+        
+    }
+    
+    
+}
---
+++ HnSMovementCross Shared/SpriteComponent.swift
@@ -0,0 +1,18 @@
+//
+//  SpriteComponent.swift
+//  HnSMovementCross
+//
+//  Created by Mauro Romito on 17/04/18.
+//  Copyright © 2018 lollibus. All rights reserved.
+//
+
+import Foundation
+import GameplayKit
+import SpriteKit
+
+class SpriteComponent: GKComponent {
+    
+    var sprite = SKSpriteNode(color: SKColor.red, size: CGSize(width. 100, height: 100))
+    
+    
+}
